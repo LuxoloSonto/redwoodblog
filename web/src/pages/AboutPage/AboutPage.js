f416293cc1fb29808bc04e6ada60f65dfb3c0a38
Luxolo Sonto <sontoluxolo17@gmail.com>
2022-06-07 23:06:20 +0200

    Day 1

---
+++ web/src/pages/AboutPage/AboutPage.js
@@ -0,0 +1,14 @@
+import { MetaTags } from '@redwoodjs/web'
+
+const AboutPage = () => {
+  return (
+    <>
+      <MetaTags title="About" description="About page" />
+
+      <h1>About Page</h1>
+      <p>This is the about page.</p>
+    </>
+  )
+}
+
+export default AboutPage
